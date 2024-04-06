25e4b2d0e5fb739ffc94df5aa2a00ef03d985e43
maksym.perevalov <maksym.perevalov@sigma.software>
2024-04-06 20:37:02 +0300

    BE: fix cors

--- Backend/db.js
+++ Backend/db.js
@@ -6,18 +6,4 @@ const repository = async (client) => {
     }
 }
 
-async function run() {
-    try {
-        await client.connect();
-        await client.db("admin").command({ping: 1});
-        const list = await client.db("publicationsdb").collection("publications").find().toArray();
-        console.log(JSON.stringify(list, null, 2));
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        await client.close();
-    }
-}
-
-run().catch(console.dir);
-
 module.exports = {repository}
--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,10 @@
-const fastify = require('fastify')({logger: true});
 const apiModule = require('./api.js')
 const dbModule = require('./db.js')
-const {MongoClient, ServerApiVersion} = require("mongodb");
 
 const uri = "mongodb+srv://root:9HIsfk70IzvvmCq6@cluster0.jcxfi04.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const initDB = async () => {
+    const {MongoClient, ServerApiVersion} = require("mongodb");
     const client = new MongoClient(uri, {
         serverApi: {
             version: ServerApiVersion.v1,
@@ -16,19 +15,29 @@ const initDB = async () => {
     return client.connect();
 }
 
+const initFastify = () => {
+    const fastify = require('fastify')({logger: true});
+    const cors = require('@fastify/cors');
+
+    fastify.register(cors, {
+        origin: '*'
+    });
+
+    return fastify;
+}
+
 const start = async () => {
     const dbClient = await initDB();
+    const fastify = initFastify();
     try {
         const repo = await dbModule.repository(dbClient);
         apiModule.api(repo, fastify);
         await fastify.listen({port: 3000});
         console.log(`Server is listening on ${fastify.server.address().port}`);
     } catch (err) {
+        await dbClient.close()
         fastify.log.error(err);
         process.exit(1);
-    } finally {
-        await fastify.close()
-        await dbClient.close()
     }
 };
 
