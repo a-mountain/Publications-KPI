214a6b09dacd8ecb98392ae7626181eaa033cf8b
maksym.perevalov <maksym.perevalov@sigma.software>
2024-04-08 20:37:01 +0300

    BE: support 2 types of data structure

--- Backend/db.js
+++ Backend/db.js
@@ -1,8 +1,14 @@
 const repository = async (client) => {
     const publications = await client.db("publicationsdb").collection("publications")
-
     return {
-
+        findPublicationsByAuthorAndDate(author, date) {
+            const predicates = []
+            if (author) predicates.push({$or: [{"issues.articles.authors.name": {$regex: author}}, {'authors.name': {$regex: author}}]})
+            if (date) predicates.push({$or: [{"issues.date": date}, {'date': date}]})
+            const query = {$and: predicates};
+            console.log(`Query ${JSON.stringify(query)}`)
+            return publications.find(query).toArray();
+        }
     }
 }
 
